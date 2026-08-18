f1bd0bc68a2407d81658c1ea9772080d887a1612
Ayush Singh <ayushcode@outlook.com>
2026-08-19 01:25:02 +0530

    completed day 1 project

---
+++ Day 1/brand_name_genrator.py
@@ -0,0 +1,6 @@
+#               THIS IS THE BRAND NAME GENERATOR PROJECT
+print("Welcome to the Brand Name Generator!")
+user_name = input("what is your name ? ")
+print("Hello " + user_name + "!")
+pet_name = input("what is your pet name ? ")
+print("Your brand name could be " + user_name + " " + pet_name)
